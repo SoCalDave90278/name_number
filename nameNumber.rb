9ee2b03be595ee3c56d426cb1d281cedffe95fb4
SoCalDave90278 <david@digitallifecyclemarketing.com>
2024-06-13 14:50:38 -0700

    Add files via upload
    Initial commit

---
+++ nameNumber.rb
@@ -0,0 +1,6 @@
+puts "Hello there, and what is your first name?"
+f_name = gets.chomp
+puts "And what is your last name?"
+l_name = gets.chomp
+var1 = f_name.length + l_name.length
+puts "Well #{f_name}, did you know your full name has #{var1} characters?"
